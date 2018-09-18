59845589fe961420819463407f6d4b64ce9433ab
Przemysław Sitek <przemo2@gmail.com>
2018-09-18 21:20:16 +0200

    init

---
+++ src/ZadanieJava46.java
@@ -0,0 +1,13 @@
+public class ZadanieJava46 {
+    public static void main(String[] args) {
+        char k, o, t;
+
+        k = (char) 75;
+        o = (char) 79;
+        t = (char) 84;
+
+        System.out.print(k);
+        System.out.print(o);
+        System.out.println(t);
+    }
+}
